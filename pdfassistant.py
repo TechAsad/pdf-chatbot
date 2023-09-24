62f90cf6b61928cd512848324350df548f8cb9a3
Muhammad Asadullah <100842006+TechAsad@users.noreply.github.com>
2023-09-24 06:52:43 +0500

    Update pdfassistant.py

--- pdfassistant.py
+++ pdfassistant.py
@@ -1,6 +1,9 @@
 import streamlit as st
 from PyPDF2 import PdfReader
 import langchain
+import docx
+import pypdf 
+import pandas as pd
 from langchain.callbacks import StreamlitCallbackHandler
 from langchain.chat_models import ChatOpenAI, ChatGooglePalm
 from langchain.chains import ConversationalRetrievalChain, LLMChain
@@ -17,7 +20,6 @@ from langchain.document_loaders.word_document import UnstructuredWordDocumentLoa
 from langchain.chains.question_answering import load_qa_chain
 
 import os
-
 
 
 import base64
@@ -67,7 +69,8 @@ api_key1 = st.secrets["GOOGLE_API_KEY"]
 #api_key2 = st.secrets["OPENAI_API_KEY"]
 os.environ["GOOGLE_API_KEY"] = api_key1
 #os.environ["OPENAI_API_KEY"] = api_key2
-
+llm = GooglePalm(temperature=0.9, max_output_tokens= 256,verbose=True,streaming=True)
+#llm = OpenAI(temperature=0.9,verbose=True)
 
 def clear_submit():
     """
@@ -94,10 +97,15 @@ def get_csv_text(file):
 
 
 # Streamlit UI
-st.set_page_config(page_title="PDF ChatBot", page_icon="books")
-st.title("Chat with your PDF")
+#st.set_page_config(page_title="Document ChatBot", page_icon="books")
+st.title("Chat with your Documents")
 
-# Define a caching function for PDF processing
+with st.sidebar:
+    #option= st.select_slider('Choose the Small or Large data Bot. Left side only works with Pdf and Docx.' ,options = ['Small Size Pdf/Docx', 'Large Size Pdf/Dcx and Csv'])
+    st.subheader("Please Choose Between")
+    use_small= st.checkbox("Small Pdf/Docx <10MB")
+    use_large= st.checkbox("Lrage Pdf/Docx and Csv")
+
 @st.cache_resource()
 def processing_pdf_docx(uploaded_file):
     # Read text from the uploaded PDF file
@@ -143,18 +151,32 @@ with st.sidebar:
 
 with st.sidebar:
     if not uploaded_file:
-        st.warning("Upload your PDF to start chatting!")
+        st.warning("Upload your files to start chatting!")
 
 
 if 'history' not in st.session_state:  
-                st.session_state['history'] = []
+        st.session_state['history'] = []
+
 
 if "messages" not in st.session_state or st.sidebar.button("Clear conversation history"):
-    st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
+    st.session_state["messages"]= []
+    st.session_state['history']  = []
 
-if uploaded_file is not None:
-    
-    document_search = process_pdf(uploaded_file)
+
+########--Main PDF--########
+
+if use_small:# option == 'Small Size Pdf/Docx':
+    use_large = False
+
+        #s= st.select_slider('Choose Pdf/docx or csv bot. Please delete csv before using pdf/docx',options = ['pdf/docx', 'csv'])
+
+
+    if uploaded_file is not None:
+        
+
+            # Define a caching function for PDF processing
+            
+            document_search = processing_pdf_docx(uploaded_file)
 
             for msg in st.session_state.messages:
                 st.chat_message(msg["role"]).write(msg["content"])
@@ -165,21 +187,21 @@ if uploaded_file is not None:
             
                 memory = ConversationBufferMemory(memory_key="chat_history", input_key="question", human_prefix= "", ai_prefix= "")
 
-        for i in range(0, len(st.session_state.messages), 2):
-            if i + 1 < len(st.session_state.messages):
-                current_message = st.session_state.messages[i]
-                next_message = st.session_state.messages[i + 1]
-                
-                current_role = current_message["role"]
-                current_content = current_message["content"]
-                
-                next_role = next_message["role"]
-                next_content = next_message["content"]
-                
-                # Concatenate role and content for context and output
-                context = f"{current_role}-replied: {current_content}\n{next_role}-asked: {next_content}"
-                
-                memory.save_context({"question": context}, {"output": ""})
+                for i in range(0, len(st.session_state.messages), 2):
+                    if i + 1 < len(st.session_state.messages):
+                        current_message = st.session_state.messages[i]
+                        next_message = st.session_state.messages[i + 1]
+                        
+                        current_role = current_message["role"]
+                        current_content = current_message["content"]
+                        
+                        next_role = next_message["role"]
+                        next_content = next_message["content"]
+                        
+                        # Concatenate role and content for context and output
+                        context = f"{current_role}: {current_content}\n{next_role}: {next_content}"
+                        
+                        memory.save_context({"question": context}, {"output": ""})
 
 
                     
@@ -216,17 +238,224 @@ if uploaded_file is not None:
                     
                 #chain = load_qa_chain(ChatOpenAI(temperature=0.9, model="gpt-3.5-turbo-0613", streaming=True) , verbose= True, prompt = PROMPT, memory=memory,chain_type="stuff")
 
-        with st.chat_message("assistant"):
-            st_cb = StreamlitCallbackHandler(st.container(), expand_new_thoughts=True)
-        
-            response = chain.run(input_documents=docs, question = prompt, callbacks=[st_cb])
-            st.session_state.messages.append({"role": "assistant", "content": response})
+                with st.chat_message("assistant"):
+                    st_cb = StreamlitCallbackHandler(st.container(), expand_new_thoughts=True)
+                
+                    response = chain.run(input_documents=docs, question = prompt, callbacks=[st_cb])
+                    st.session_state.messages.append({"role": "Assistant", "content": response})
+                    
+                    st.write(response)
+
+
+@st.cache_resource()
+def processing_csv_pdf_docx(uploaded_file2):
+    # Read text from the uploaded PDF file
+    data = []
+    for file in uploaded_file2:
+        split_tup = os.path.splitext(file.name)
+        file_extension = split_tup[1]
+    
+        if file_extension == ".pdf":
+
+            with tempfile.NamedTemporaryFile(delete=False) as tmp_file1:
+                tmp_file1.write(file.getvalue())
+                tmp_file_path1 = tmp_file1.name
+                loader = PyPDFLoader(file_path=tmp_file_path1)
+                documents = loader.load()
+                text_splitter = CharacterTextSplitter(chunk_size=200, chunk_overlap=10)
+                data += text_splitter.split_documents(documents)
+
+
+        if file_extension == ".csv":
             
-            st.write(response)
+            with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
+                tmp_file.write(file.getvalue())
+                tmp_file_path = tmp_file.name
+
+                loader = CSVLoader(file_path=tmp_file_path, encoding="utf-8", csv_args={
+                            'delimiter': ','})
+                documents = loader.load()
+                
+                text_splitter = CharacterTextSplitter(chunk_size=200, chunk_overlap=10)
+    
+                data += text_splitter.split_documents(documents)
+                st.sidebar.header(f"Data-{file.name}")
+                data1 = pd.read_csv(tmp_file_path)
+                st.sidebar.dataframe(data1)
+        
+        if file_extension == ".docx":
+
+            with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
+                tmp_file.write(file.getvalue())
+                tmp_file_path = tmp_file.name
+                loader = UnstructuredWordDocumentLoader(file_path=tmp_file_path)
+                documents = loader.load()
+                text_splitter = CharacterTextSplitter(chunk_size=200, chunk_overlap=10)
+
+                data += text_splitter.split_documents(documents)
+            
+
+    # Download embeddings from GooglePalm
+    embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
+    #embeddings = GooglePalmEmbeddings()
+    #embeddings = OpenAIEmbeddings()
+
+    # Create a FAISS index from texts and embeddings
+    vectorstore = FAISS.from_documents(data, embeddings)
+
+    return vectorstore
+
+
+########--Main CSV--########
+
+
+if use_large: #option == 'Large Size Pdf/Dcx and Csv':
+    use_small = False
+    uploaded_file2 = uploaded_file
+    
+        #s= st.select_slider('Choose Pdf/docx or csv bot. Please delete csv before using pdf/docx',options = ['pdf/docx', 'csv'])
+
+
+    if uploaded_file2:
+
+        vectorstore = processing_csv_pdf_docx(uploaded_file2)
+
+        for msg in st.session_state.messages:
+            st.chat_message(msg["role"]).write(msg["content"])
+        
+        if prompt := st.chat_input(placeholder="Ask Me!"):
+            st.session_state.messages.append({"role": "user", "content": prompt})
+            st.chat_message("user").write(prompt)
+        
+            memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True, input_key="question", human_prefix= "", ai_prefix= "")
+
+            for i in range(0, len(st.session_state.messages), 2):
+                if i + 1 < len(st.session_state.messages):
+                    current_message = st.session_state.messages[i]
+                    next_message = st.session_state.messages[i + 1]
+                    
+                    current_role = current_message["role"]
+                    current_content = current_message["content"]
+                    
+                    next_role = next_message["role"]
+                    next_content = next_message["content"]
+                    
+                    # Concatenate role and content for context and output
+                    context = f"{current_content}\n{next_role}:{next_content}"
+                    
+                    memory.save_context({"question": context}, {"output": ""})
+            
+            general_system_template = r""" 
+            -You are data analyst and a helpful assistant. You have been provided with information of companies.
+            -chat humbly, and answer honestly. Answer the question from provided data only, do not make up any answer outside the provided data.
+            -Use the following pieces of context to answer the question at the end. 
+            -Your answer should not exceed 15 words.
+            - Do not write "Assistant" in response
+            -SEK and DKK are currencies.
+
+            -If you don't know the answer, just say I don't know. Do not make up any answer.
+            
+            Example1 :
+
+            Human: What are the number of sales for A and B company in 2020?
+
+            Assistant: The number of sale for A are 2458 and for B are 198 in 2020.
+
+            Example 2:
+
+            Human: sales and net profit of alcadon in 2020?
+
+            Assistant: Sales:467600, Net profit:29000
+
+            -Following is the relevant data:
+            ----
+
+            {context}
+
+            ----
+
+            Chat History:
+
+            ------
+            {chat_history}
+            ------
+
+            """
+            general_user_template ="``{question}``"
+            messages = [
+                        SystemMessagePromptTemplate.from_template(general_system_template), HumanMessagePromptTemplate.from_template(general_user_template)
+            ]
+            qa_prompt = ChatPromptTemplate.from_messages( messages )
+
+
+            general_system_template2 = r""" 
+        Given the following conversation and a follow up question, only rephrase the follow up question a little to be a standalone question, in its original language...
+
+Example1:
+Conversation:
+User: Please provide the sales data for Q2 2023.
+Assistant: Here's the sales data for Q2 2023.
+
+User: And can you also share the profit figures?
+
+Rephrased Standalone Question: 
+Human: Please share the profit figures for Q2 2023.
+
+Example 2:
+Conversation:
+user: What are the top-selling products in the last month?
+Assistant: The top-selling products last month were A, B, and C.
+User: Could you list the top-performing products in terms of customer reviews as well?
 
 
 
+Rephrased Standalone Question:
+User: Please list the top-performing products in terms of customer reviews.
+
+Wrong response: 
+Sure, here is the rephrased standalone question: Human:
+
+        Conversation:
+
+        ------
+        {chat_history}
+        ------
+        Human: {question}
+
+        """
+            general_user_template2 ="``{question}``"
+            qa_prompt2 = ChatPromptTemplate.from_template( general_system_template2 )
+
+            llm2 = GooglePalm(temperature=0.5, verbose=True)
 
 
+                # Load question-answering chain
+            chain = ConversationalRetrievalChain.from_llm(  
+            llm , verbose= True, memory = memory, 
+            retriever=vectorstore.as_retriever(), max_tokens_limit=512#,condense_question_prompt= qa_prompt2, condense_question_llm=llm2
+ ,combine_docs_chain_kwargs={'prompt':qa_prompt})
+            #chain = ConversationalRetrievalChain.from_llm(GooglePalm(temperature=0.5), verbose= True, prompt = PROMPT,memory=memory, chain_type="stuff")
+                
+            #chain = load_qa_chain(ChatOpenAI(temperature=0.9, model="gpt-3.5-turbo-0613", streaming=True) , verbose= True, prompt = PROMPT, memory=memory,chain_type="stuff")
+            
+
+            with st.chat_message("assistant"):
+                st_cb = StreamlitCallbackHandler(st.container(), expand_new_thoughts=True)
+            
+                response = chain({"question": prompt,   
+                                        "chat_history": st.session_state['history']}, callbacks=[st_cb])
+                st.session_state.messages.append({"role": "Assistant", "content": response['answer']})
+                st.session_state['history'].append((prompt, response["answer"])) 
+                st.write(response['answer'])
+
+
+
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
     
 
