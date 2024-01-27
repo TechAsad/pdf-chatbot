76ebd5b75e464560f1d987c0280f51c983659bf4
Muhammad Asadullah <100842006+TechAsad@users.noreply.github.com>
2024-01-27 02:42:21 +0000

    app updated

--- pdfassistant.py
+++ pdfassistant.py
@@ -71,7 +71,7 @@ else:
 
 @st.cache_resource(show_spinner=False)
 def processing_csv_pdf_docx(uploaded_file):
-    with st.spinner(text="Getting Ready"):
+    with st.spinner(text="Embedding Your Files"):
 
         # Read text from the uploaded PDF file
         data = []
@@ -161,7 +161,7 @@ def load_files():
 
 
 def main():
-   # try:
+    try:
         if (use_openai and openai_api_key) or use_google:
             if uploaded_file:
                 load_files()
@@ -268,13 +268,12 @@ def main():
                                                 
                                 st.write(response)
                                 
-    #except Exception as e:
-     #   "Sorry, there was a problem."
-      #  if use_google:
-      
-       #     "Google PaLM AI only take English Data and Questions. Or the AI could not find the answer in your provided document."
-        #elif use_openai:
-         #   "Please check your OpenAI API key"
+    except Exception as e:
+        "Sorry, there was a problem. A corrupted file or;"
+        if use_google:
+            "Google PaLM AI only take English Data and Questions. Or the AI could not find the answer in your provided document."
+        elif use_openai:
+            "Please check your OpenAI API key"
          
 
 
