c016838c492e6b46ed2f8f442fd6066af3f47df4
Muhammad Asadullah <100842006+TechAsad@users.noreply.github.com>
2024-01-27 01:05:53 +0000

    app updated

--- pdfassistant.py
+++ pdfassistant.py
@@ -140,130 +140,121 @@ if not uploaded_file:
     st.warning("Upload your file(s) to start chatting!")
     
 
-
 if 'history' not in st.session_state:  
         st.session_state['history'] = []
-
 
 if "messages" not in st.session_state or st.sidebar.button("Clear conversation history"):
     st.session_state["messages"]= []
     st.session_state['history']  = []
 
 
-########--Main PDF--########
-def load_files():
-    for file in uploaded_file:
-        with open(os.path.join('uploaded_files', file.name), 'wb') as f:
-            f.write(file.getbuffer())
-            st.success(f'Saved file: {file.name}', icon="✅")
-    
-
-
 def main():
    # try:
         if (use_openai and openai_api_key) or use_google:
             if uploaded_file:
-                vectorstore= processing_csv_pdf_docx(uploaded_file)
-        
-            for msg in st.session_state.messages:
-                st.chat_message(msg["role"]).write(msg["content"])      
+                for file in uploaded_file:
+                    vectorstore= processing_csv_pdf_docx(uploaded_file)
+                    st.success(f'File Embedded: {file.name}', icon="✅")
             
-            if prompt := st.chat_input(placeholder="Type your question!"):
-                st.session_state.messages.append({"role": "user", "content": prompt})
-                st.chat_message("user").write(prompt)
-                memory = ConversationBufferMemory(memory_key="chat_history", input_key="question", human_prefix= "", ai_prefix= "")
-                user_message = {"role": "user", "content": prompt}
+                for msg in st.session_state.messages:
+                    st.chat_message(msg["role"]).write(msg["content"])      
                 
-                
-                for i in range(0, len(st.session_state.messages), 2):
-                    if i + 1 < len(st.session_state.messages):
-                        user_prompt = st.session_state.messages[i]
-                        ai_res = st.session_state.messages[i + 1]
-                        
-                        current_role = user_prompt["role"]
-                        current_content = user_prompt["content"]
-                        
-                        next_role = ai_res["role"]
-                        next_content = ai_res["content"]
-                        
-                        # Concatenate role and content for context and output
-                        user = f"{current_role}: {current_content}"
-                        ai = f"{next_role}: {next_content}"
-                        
-                        memory.save_context({"question": user}, {"output": ai})
-
-                # Get user input -> Generate the answer
-                greetings = ['Hey', 'Hello', 'hi', 'hello', 'hey', 'helloo', 'hellooo', 'g morning', 'gmorning', 'good morning', 'morning',
-                            'good day', 'good afternoon', 'good evening', 'greetings', 'greeting', 'good to see you',
-                            'its good seeing you', 'how are you', "how're you", 'how are you doing', "how ya doin'", 'how ya doin',
-                            'how is everything', 'how is everything going', "how's everything going", 'how is you', "how's you",
-                            'how are things', "how're things", 'how is it going', "how's it going", "how's it goin'", "how's it goin",
-                            'how is life been treating you', "how's life been treating you", 'how have you been', "how've you been",
-                            'what is up', "what's up", 'what is cracking', "what's cracking", 'what is good', "what's good",
-                            'what is happening', "what's happening", 'what is new', "what's new", 'what is neww', "g’day", 'howdy']
-                compliment = ['thank you', 'thanks', 'thanks a lot', 'thanks a bunch', 'great', 'ok', 'ok thanks', 'okay', 'great', 'awesome', 'nice']
+                if prompt := st.chat_input(placeholder="Type your question!"):
+                    st.session_state.messages.append({"role": "user", "content": prompt})
+                    st.chat_message("user").write(prompt)
+                    memory = ConversationBufferMemory(memory_key="chat_history", input_key="question", human_prefix= "", ai_prefix= "")
+                    user_message = {"role": "user", "content": prompt}
+                    
+                    
+                    for i in range(0, len(st.session_state.messages), 2):
+                        if i + 1 < len(st.session_state.messages):
+                            user_prompt = st.session_state.messages[i]
+                            ai_res = st.session_state.messages[i + 1]
                             
-                prompt_template =dedent(r"""
-                You are a helpful assistant to help students study from their study material.
-                talk humbly. Answer my question from the provided context. Do not answer from your own training data.
-                Use the following pieces of context to answer the question at the end.
-                If you don't know the answer, just say that you don't know. Do not makeup any answer.
-                Do not answer hypothetically.
-                Please Do Not say: "Based on the provided context"
-                Always use the context to find the answer.
-                
-                this is the context from study material:
-                ---------
-                {context}
-                ---------
+                            current_role = user_prompt["role"]
+                            current_content = user_prompt["content"]
+                            
+                            next_role = ai_res["role"]
+                            next_content = ai_res["content"]
+                            
+                            # Concatenate role and content for context and output
+                            user = f"{current_role}: {current_content}"
+                            ai = f"{next_role}: {next_content}"
+                            
+                            memory.save_context({"question": user}, {"output": ai})
 
-                Current Conversation: 
-                ---------
-                {chat_history}
-                ---------
-
-                New Question: {question}
-
-                Helpful Answer: 
-                """)
-                
-                
-
-                PROMPT = PromptTemplate(
-                    template=prompt_template, input_variables=["context", "question", "chat_history"]
-                )
-
-                # Run the question-answering chain
-                
-                
-                
-                    # Load question-answering chain
-                chain = load_qa_chain(llm=llm, verbose= True, prompt = PROMPT,memory=memory, chain_type="stuff")
+                    # Get user input -> Generate the answer
+                    greetings = ['Hey', 'Hello', 'hi', 'hello', 'hey', 'helloo', 'hellooo', 'g morning', 'gmorning', 'good morning', 'morning',
+                                'good day', 'good afternoon', 'good evening', 'greetings', 'greeting', 'good to see you',
+                                'its good seeing you', 'how are you', "how're you", 'how are you doing', "how ya doin'", 'how ya doin',
+                                'how is everything', 'how is everything going', "how's everything going", 'how is you', "how's you",
+                                'how are things', "how're things", 'how is it going', "how's it going", "how's it goin'", "how's it goin",
+                                'how is life been treating you', "how's life been treating you", 'how have you been', "how've you been",
+                                'what is up', "what's up", 'what is cracking', "what's cracking", 'what is good', "what's good",
+                                'what is happening', "what's happening", 'what is new', "what's new", 'what is neww', "g’day", 'howdy']
+                    compliment = ['thank you', 'thanks', 'thanks a lot', 'thanks a bunch', 'great', 'ok', 'ok thanks', 'okay', 'great', 'awesome', 'nice']
+                                
+                    prompt_template =dedent(r"""
+                    You are a helpful assistant to help students study from their study material.
+                    talk humbly. Answer my question from the provided context. Do not answer from your own training data.
+                    Use the following pieces of context to answer the question at the end.
+                    If you don't know the answer, just say that you don't know. Do not makeup any answer.
+                    Do not answer hypothetically.
+                    Please Do Not say: "Based on the provided context"
+                    Always use the context to find the answer.
                     
-                #chain = load_qa_chain(ChatOpenAI(temperature=0.9, model="gpt-3.5-turbo-0613", streaming=True) , verbose= True, prompt = PROMPT, memory=memory,chain_type="stuff")
+                    this is the context from study material:
+                    ---------
+                    {context}
+                    ---------
 
-                with st.chat_message("assistant"):
-                    st_cb = StreamlitCallbackHandler(st.container())
-                    if prompt.lower() in greetings:
-                        response = 'Hi, how are you? I am here to help you get information from your file. How can I assist you?'
-                        st.session_state.messages.append({"role": "Assistant", "content": response})
-                        st.write(response)
-                    elif prompt.lower() in compliment:
-                        response = 'My pleasure! If you have any more questions, feel free to ask.'
-                        st.session_state.messages.append({"role": "Assistant", "content": response})
-                        st.write(response)
-                    else:
-                        with st.spinner('Bot is typing ...'):
-                            docs = vectorstore.similarity_search(prompt, k=5, fetch_k=50)
+                    Current Conversation: 
+                    ---------
+                    {chat_history}
+                    ---------
+
+                    New Question: {question}
+
+                    Helpful Answer: 
+                    """)
                     
-                            response = chain.run(input_documents=docs, question = prompt)#, callbacks=[st_cb])
+                    
+
+                    PROMPT = PromptTemplate(
+                        template=prompt_template, input_variables=["context", "question", "chat_history"]
+                    )
+
+                    # Run the question-answering chain
+                    
+                    
+                    
+                        # Load question-answering chain
+                    chain = load_qa_chain(llm=llm, verbose= True, prompt = PROMPT,memory=memory, chain_type="stuff")
+                        
+                    #chain = load_qa_chain(ChatOpenAI(temperature=0.9, model="gpt-3.5-turbo-0613", streaming=True) , verbose= True, prompt = PROMPT, memory=memory,chain_type="stuff")
+
+                    with st.chat_message("assistant"):
+                        st_cb = StreamlitCallbackHandler(st.container())
+                        if prompt.lower() in greetings:
+                            response = 'Hi, how are you? I am here to help you get information from your file. How can I assist you?'
                             st.session_state.messages.append({"role": "Assistant", "content": response})
-                            
-                            assistant_message = {"role": "assistant", "content": response}
-                        
-                                            
                             st.write(response)
+                        elif prompt.lower() in compliment:
+                            response = 'My pleasure! If you have any more questions, feel free to ask.'
+                            st.session_state.messages.append({"role": "Assistant", "content": response})
+                            st.write(response)
+                        else:
+                            with st.spinner('Bot is typing ...'):
+                                docs = vectorstore.similarity_search(prompt, k=5, fetch_k=50)
+                        
+                                response = chain.run(input_documents=docs, question = prompt)#, callbacks=[st_cb])
+                                st.session_state.messages.append({"role": "Assistant", "content": response})
+                                
+                                assistant_message = {"role": "assistant", "content": response}
                             
+                                                
+                                st.write(response)
+                                
    # except Exception as e:
     #    "Sorry, there was a problem."
      #   if use_google:
